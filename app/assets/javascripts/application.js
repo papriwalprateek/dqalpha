f217bd6fe022ca88da52f0c855e0cec90e2fb171
vinayrajchoudhary <vinayrajchoudhary@gmail.com>
2012-11-26 03:32:45 +0530

    wiki image and quest link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,7 +72,11 @@ $(function() {
     window.history.pushState(null, document.title, this.href);
      return false;
   });
-  
+  $("#q text.grey a").live("click", function() {
+    $.getScript(this.href);
+    window.history.pushState(null, document.title, this.href);
+     return false;
+  });
    $(window).bind("popstate", function() {
       $.getScript(location.href);
     });
