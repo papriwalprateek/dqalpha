d5bcc43ed6bf9306b32c2eecbf2cfc1419d22bbd
vinayrajchoudhary <vinayrajchoudhary@gmail.com>
2012-12-31 18:33:45 +0530

    poll options added successfully
    voting up is yet to be added

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
-  attr_accessible :content, :poll_id
+  attr_accessible :content, :poll_id, :options_attributes
   belongs_to :poll
   has_many :options, :dependent => :destroy
-   
+  accepts_nested_attributes_for :options
+
 end
