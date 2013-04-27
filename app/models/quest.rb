f96c33de84886a9534f4fd170ec8027ae6eaf6c3
vinayrajchoudhary <vinayrajchoudhary@gmail.com>
2013-04-27 13:57:37 +0530

    deleting quests with all their qs

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,7 +1,7 @@
 class Quest < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :involvements
-  has_many :qs
+  has_many :qs, :dependent => :destroy
   has_many :links
   has_many :users, :through => :involvements, :uniq => true 
 end
