3ae1a4bcc7870392b87f83b38789e95f0c6222f9
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2013-08-23 17:33:30 +0530

    pseudo commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,7 @@
 // the compiled file.
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
+// GO AFTER THE REQUIRES BELOW
 //
 //= require jquery
 //= require jquery-migrate
