98acc26db2f28dabd841f54a163bc59c114d2beb
Vinay Raj Choudhary <vinayrajchoudhary@gmail.com>
2014-02-12 12:00:01 +0530

    .

--- app/assets/javascripts/algorithm.js
+++ app/assets/javascripts/algorithm.js
@@ -81,7 +81,7 @@ function back_navigation(){
 	$('div.quest_search_page .vm_results').hide();
 	$('div.quest_search_page #'+vm_live_id).show();
 }
-	_gaq.push(['_setDomainName', 'daqwest.com']);
+	_gaq.push(['_setDomainName', '.daqwest.com']);
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
